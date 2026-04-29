816dfb127962eabcebaf44739be67ca0d81aeef3
parkash34 <ohm.parkash34@gmail.com>
2026-04-30 00:12:26 +0200

    Updated it

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel, field_validator
+from typing import Optional
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from pinecone import Pinecone, ServerlessSpec
+from langchain_core.messages import HumanMessage
 from langchain_groq import ChatGroq
+
